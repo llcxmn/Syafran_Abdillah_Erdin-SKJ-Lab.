e3ad088a9702dbc50ce89337cffa701d76c14eb7
Syafran Abdillah Erdin <145352692+llcxmn@users.noreply.github.com>
2024-09-18 14:28:50 +0700

    Update first_config.py

--- Assigment 2/first_config.py
+++ Assigment 2/first_config.py
@@ -1,1 +1,9 @@
-dump
+from gem5.components.boards.simple_board import SimpleBoard
+from gem5.components.cachehierarchies.classic.no_cache import NoCache
+from gem5.components.memory import SingleChannelDDR3_1600
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.resources.resource import obtain_resource
+from gem5.simulate.simulator import Simulator
+from gem5.isas import ISA
+
